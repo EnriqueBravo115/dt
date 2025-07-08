228fbf0f38f2b17d062a0b43bb0c52243d33c84a
EnriqueBravo115 <enriquebravo115@gmail.com>
2025-07-08 14:23:06 -0600

    markdow preview fix

--- .config/nvim/lua/custom/plugins/preview.lua
+++ .config/nvim/lua/custom/plugins/preview.lua
@@ -2,9 +2,13 @@ return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
+  build = function()
+    vim.fn["mkdp#util#install"]()
+  end,
   config = function()
-    vim.cmd("let g:mkdp_highlight_css='/home/nullboy/.config/nvim/resources/my.css'")
-    vim.cmd("let g:mkdp_theme='light'")
-    vim.cmd("let g:mkdp_page_title = '${name}'")
+    vim.g.mkdp_highlight_css = "/home/nullboy/.config/nvim/resources/my.css"
+    vim.g.mkdp_theme = "light"
+    vim.g.mkdp_page_title = "${name}"
+    vim.g.mkdp_browser = "firefox"
   end
 }
