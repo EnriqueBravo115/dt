1ba5383521af1476fc1722baa44bdef3a7c0b6a1
EnriqueBravo115 <enriquebravo115@gmail.com>
2025-09-09 00:43:26 -0600

    change colorscheme

--- .config/nvim/lua/custom/plugins/colorscheme.lua
+++ .config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,38 +1,26 @@
 return {
-  "vague2k/vague.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("vague").setup({
-      transparent = true,
-      bold = true,
-      italic = false,
-      style = {
-        boolean = "bold",
-        number = "none",
-        float = "none",
-        error = "bold",
-        comments = "none",
-        conditionals = "none",
-        functions = "none",
-        headings = "bold",
-        operators = "none",
-        strings = "none",
-        variables = "none",
-
-        -- keywords
-        keywords = "none",
-        keyword_return = "none",
-        keywords_loop = "none",
-        keywords_label = "none",
-        keywords_exception = "none",
-
-        -- builtin
-        builtin_constants = "bold",
-        builtin_functions = "none",
-        builtin_types = "bold",
-        builtin_variables = "none",
-      },
-    })
-  end
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("solarized-osaka").setup({
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+          comments = { italic = false },
+          keywords = { italic = false },
+          functions = {},
+          variables = {},
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+        sidebars = { "qf", "help" },
+        day_brightness = 0.3,
+        hide_inactive_statusline = false,
+        dim_inactive = false,
+        lualine_bold = true,
+      })
+    end
+  },
 }
--- .config/nvim/lua/custom/plugins/various.lua
+++ .config/nvim/lua/custom/plugins/various.lua
@@ -1,11 +1,14 @@
 return {
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup()
+    end
+  },
+
   "mbbill/undotree",
   "tpope/vim-fugitive",
   "tpope/vim-surround",
   "mfussenegger/nvim-jdtls",
-  "norcalli/nvim-colorizer.lua",
   "ThePrimeagen/harpoon",
-  config = function()
-    require("colorizer").setup()
-  end
 }
